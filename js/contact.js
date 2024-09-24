bfb34a21c4f9087ed573ee228b68567b9c040cc7
AjithCnuts <ajith@creativenuts.dev>
2024-09-24 16:57:52 +0530

    contact us page , blog, service

--- js/contact.js
+++ js/contact.js
@@ -1,17 +1,16 @@
-	
-   var map;
-    map = new GMaps({
-        el: '#map',
-        lat: 21.2334329,
-        lng: 72.86372,
-        scrollwheel: false
-    });
+var map;
+map = new GMaps({
+    el: '#map',
+    lat: 12.9131013,  // Latitude for Bangalore location
+    lng: 77.6324936,  // Longitude for Bangalore location
+    scrollwheel: false
+});
 
-    map.addMarker({
-        lat: 21.2334329,
-        lng: 72.86372,
-        title: 'Marker with InfoWindow',
-        infoWindow: {
-            content: '<p>Idea Homes Melbourne, Merrick Way, <br>FL 12345 Australia<a href="#"  target="_blank">Themeforest</a></p>'
-        }
-    });    
+map.addMarker({
+    lat: 12.9131013,  // Latitude for Bangalore marker
+    lng: 77.6324936,  // Longitude for Bangalore marker
+    title: 'Marker with InfoWindow',
+    infoWindow: {
+        content: '<p>Uniquebuildss Construction & Interiors Company,<br>Bangalore, India</p>'
+    }
+});
